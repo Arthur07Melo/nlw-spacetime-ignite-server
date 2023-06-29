7215aa11a6c17c34802be80d3ba7cb7a0ef27cc3
Arthur07Melo <aarthur00melo@gmail.com>
2023-06-29 12:27:01 -0300

    jwt middleware added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { expressjwt } from "express-jwt";
 
 import user from "./routes/user";
 import memory from "./routes/memory";
@@ -13,11 +16,11 @@ app.use(cors({
     origin: ["http://localhost:3000"]
 }));
 
-
 app.use(express.json());
 app.use(helmet());
 
 app.use("/", register);
+app.use(expressjwt({ secret: process.env.TOKEN_SECRET_KEY ?? "", algorithms: ["HS256"] }));
 app.use("/user", user);
 app.use("/memories", memory);
 
